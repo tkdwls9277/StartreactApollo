50d3c403daa2fb70269527e1831d90a715e50f21
LeeSangJin <tkdwls9277@naver.com>
2020-11-21 15:11:32 +0900

    More Mutations and Updating the Store

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag'
 import React, { Component } from 'react'
 import Link from './Link'
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -11,10 +11,21 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `
+
 class LinkList extends Component {
   render() {
     return (
@@ -24,16 +35,33 @@ class LinkList extends Component {
           if (error) return <div>Error</div>
     
           const linksToRender = data.feed.links
-    
+
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+                <Link
+                key={link.id}
+                link={link}
+                index={index}
+                updateStoreAfterVote={this._updateCacheAfterVote}
+              />
+              ))}
             </div>
           )
         }}
       </Query>
     )
   }
+  
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
 }
+
 
 export default LinkList
--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 //import * as serviceWorker from './serviceWorker';
 
 // 1
@@ -9,6 +12,16 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 
 
 // 2
@@ -18,15 +31,17 @@ const httpLink = createHttpLink({
 
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
 // serviceWorker.unregister();
