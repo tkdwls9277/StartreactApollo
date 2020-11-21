b87afa36d7f8769794c7c064b7033badbb7589b6
LeeSangJin <tkdwls9277@naver.com>
2020-11-21 17:07:19 +0900

    frontend end

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -5,6 +5,9 @@ import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 //import * as serviceWorker from './serviceWorker';
 
 // 1
@@ -29,9 +32,27 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
-// 3
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
