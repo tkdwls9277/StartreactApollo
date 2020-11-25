b99d97f6f935d86286956dd2c347602ff094bce0
LeeSangJin <tkdwls9277@naver.com>
2020-11-25 23:23:01 +0900

    simple mutation

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -7,6 +7,10 @@ const typeDefs = `
     feed: [Link!]!
   }
 
+  type Mutation {
+    post(url: String!, description: String!): Link!
+  }
+
   type Link {
     id: ID!
     description: String!
@@ -14,16 +18,37 @@ const typeDefs = `
   }
 `
 
-// 2
+// 1
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+// 1
+let idCount = links.length
 const resolvers = {
   Query: {
-    info: () => null
-  }
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
