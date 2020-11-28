94b57001d863b7de1b08fdb5d2c74b7b65c896b3
LeeSangJin <tkdwls9277@naver.com>
2020-11-28 23:28:42 +0900

    서버와 데이터베이스 연동

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
 // 1
 const typeDefs = `
@@ -25,30 +26,33 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
-// 1
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
   },
   Mutation: {
-    // 2
-    post: (parent, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
   },
 }
 
-// 3
+const prisma = new PrismaClient()
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {
+    prisma,
+  }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
