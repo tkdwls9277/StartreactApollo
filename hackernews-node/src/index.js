c66ea5f3a917da900e19626412855f9247e88cd3
LeeSangJin <tkdwls9277@naver.com>
2020-12-03 23:04:40 +0900

    리졸버

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -51,8 +51,12 @@ const prisma = new PrismaClient()
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: {
-    prisma,
-  }
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
