780b46435c28f196bd8311cde34e79a27bacc6d8
LeeSangJin <tkdwls9277@naver.com>
2021-01-23 17:48:02 +0900

    코테

--- programmers/network.js
+++ programmers/network.js
@@ -1,6 +1,17 @@
 function solution(n, computers) {
     var answer = 0;
-    
+    var visited = Array.from({length: computers.length}, (v, i) => false);
+
+    function dfs(index){
+        
+    }
+
+    for(var i=0;i<n;i++){
+        if(!visited[i]){
+            dfs(i);
+            answer++;
+        }
+    }
     return answer;
 }
 
