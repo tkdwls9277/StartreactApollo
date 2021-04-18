d77967006c5e7a8bb866110aa36fc9b930b56075
LeeSangJin <tkdwls9277@naver.com>
2021-04-18 18:31:39 +0900

    리액트 공부

---
+++ react-study/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>hihi</div>
+      <div>hello react</div>
+    </div>
+  );
+}
+
+export default App;
